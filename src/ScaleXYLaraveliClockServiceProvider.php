55dcb73df5c3fb5d532a8d1a9b21f8d2db0a8e3e
Athul Raj <mail@athulraj.com>
2024-07-07 18:21:08 +0530

    feat: Add ScaleXYLaraveliClockServiceProvider class

---
+++ src/ScaleXYLaraveliClockServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ScaleXY\iClock;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScaleXYLaraveliClockServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
